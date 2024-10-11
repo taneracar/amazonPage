155f58d3f5dd8dac08c33defc6df0103273ede71
taneracar <tanerrr.acar@gmail.com>
2024-10-11 23:04:53 +0300

    generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,18 @@
-export const cart = [];
-export function addToCart(productID) {
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
+
+export function addToCart(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
-    if (productID === cartItem.productID) {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
@@ -10,7 +20,7 @@ export function addToCart(productID) {
     matchingItem.quantity += 1;
   } else {
     cart.push({
-      productID,
+      productId,
       quantity: 1,
     });
   }
