69669af3145068b4dacc3f105a15b461ad8bd416
taneracar <tanerrr.acar@gmail.com>
2024-10-11 23:50:19 +0300

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,9 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  cartSummaryHTML += ` <div class="cart-item-container">
+  cartSummaryHTML += ` <div class="cart-item-container js-cart-item-container${
+    matchingProduct.id
+  }">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -96,6 +98,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container${productId}`
+    );
+    container.remove();
   });
 });
