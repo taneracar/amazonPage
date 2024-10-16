2cedee3dd6759088094896c371f788227982b15f
taneracar <tanerrr.acar@gmail.com>
2024-10-16 18:46:32 +0300

    create automated tests for addToCart

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 describe("test suite: formatCurrency", () => {
   it("converts cent into dolars", () => {
     expect(formatCurrency(2095)).toEqual("20.95");
